9dfa3bc2e62fce646c38a0a5295cc0cceae121f0
yvandy <vandanagarg205@gmail.com>
2020-12-12 17:19:24 +0530

    Array reduce changes

--- Day 1/variable.js
+++ Day 1/variable.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+const arr = [1,2,3,4,5];
+
+// testing array reduce
+function sum(arr){
+    return arr.reduce((prev,current)=>prev+current,0);
+}
+console.log(sum(arr));
