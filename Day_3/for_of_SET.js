7a757a2a960e3842125c4e6f90b2e041290e9a78
yvandy <vandanagarg205@gmail.com>
2020-12-14 06:54:03 +0530

    for_of on SET

---
+++ Day_3/for_of_SET.js
@@ -0,0 +1,7 @@
+let text = "Na na na na na na na na Batman!";
+let wordSet = new Set(text.split( " "));
+let unique = [];
+for (let word of wordSet){
+    unique.push(word);
+}
+console.log(unique);
