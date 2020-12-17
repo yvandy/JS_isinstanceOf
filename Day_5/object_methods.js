46d730a1c9bdef04c8b33eb442d33ac87981177f
yvandy <vandanagarg205@gmail.com>
2020-12-17 18:46:41 +0530

    Object Mehods

--- Day_5/object_methods.js
+++ Day_5/object_methods.js
@@ -3,4 +3,18 @@ x :1,
 y:2,
 toString: function() { return `(${this.x},${this.y})`;}
 };
-String(point);
+console.log(String(point));
+
+// valueOf() method  //
+
+let pp = {
+    x:3,
+    y:4,
+    valueOf: function(){
+        return Math.hypot(this.x,this.y);
+    }
+}
+console.log(Number(pp));
+console.log(pp>4);
+console.log(pp>5);
+console.log(pp<6);
