84eba49760ed57ce124a4a6b452620c85b1b3782
yvandy <vandanagarg205@gmail.com>
2020-12-19 11:21:59 +0530

    Arrays

---
+++ Day_7/arrays.js
@@ -0,0 +1,9 @@
+let a = ["world"]; // Start with a one-element array
+let value = a[0]; // Read element 0
+a[1] = 3.14; // Write element 1
+let i = 2;
+a[i] = 3; // Write element 2
+a[i + 1] = "hello"; // Write element 3
+a[a[i]] = a[0]; 
+
+console.log(a);
