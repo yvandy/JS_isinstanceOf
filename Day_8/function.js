78f7ebbe8e100dc20ae47d8101c1b35193d3fef4
yvandy <vandanagarg205@gmail.com>
2020-12-24 22:16:01 +0530

    functions_1

---
+++ Day_8/function.js
@@ -0,0 +1,15 @@
+
+let o = {
+    m: function(){
+        let self= this;
+        console.log(this === o);
+
+        f();
+
+        function f(){
+            console.log(this === o);
+            console.log(self === o);
+        }
+    }
+}
+o.m();
