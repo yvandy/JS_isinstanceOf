a1ba11a2eb0a5c71449e3ee7e4bc0d56f98670a2
yvandy <vandanagarg205@gmail.com>
2020-12-31 17:03:14 +0530

    inheritance

--- Day_10/classes1.js
+++ Day_10/classes1.js
@@ -19,6 +19,27 @@ class Employee{
         return a+b;
     }
 }
+
+class Programmer extends Employee{
+    constructor(givenName,givenExp,givenDivision,language,github){
+        super(givenName,givenExp,givenDivision);
+        this.language = language;
+        this.github = github;
+}
+
+favoriteLanguage(){
+    if(this.language == "python"){
+        return 'python';
+    }
+    else {
+        return "javascript";
+    }
+}
+static multiply(a,b){
+
+    return a*b;
+}
+}
 obj1 = new Employee("vandana",1,"B");
 console.log(obj1.slogan());
 console.log(obj1.joiningYear());
