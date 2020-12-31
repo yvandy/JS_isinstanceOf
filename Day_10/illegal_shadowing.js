ffba798857336865368b12d4b2479846b63af745
yvandy <vandanagarg205@gmail.com>
2021-01-01 01:54:51 +0530

    illegal shadwing

---
+++ Day_10/illegal_shadowing.js
@@ -0,0 +1,6 @@
+let a =20;
+{
+    var a= 20; //trying to shadow a let variable inside a block using var variable is not possible, this is illegal shadowing
+    // but trying to shadow a let variable using a let varible is=nside a block is totally fine.
+
+}
