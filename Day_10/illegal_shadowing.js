e1b83a2220a153fed001b3ac4a1f601d32f2a361
yvandy <vandanagarg205@gmail.com>
2021-01-01 02:02:41 +0530

    legal and illegal shadowing

--- Day_10/illegal_shadowing.js
+++ Day_10/illegal_shadowing.js
@@ -1,6 +1,15 @@
 let a =20;
 {
     var a= 20; //trying to shadow a let variable inside a block using var variable is not possible, this is illegal shadowing
-    // but trying to shadow a let variable using a let varible is=nside a block is totally fine.
+    // but trying to shadow a let variable using a let varible inside a block is totally fine.
+
+    // But vice versa is legal like: trying to shadow a var varibale inside a block using let variable is legal
+    /*
+        var a =20;
+        {
+                let a = 20;
+        }
+
+    */
 
 }
