808f1113ce68146ee305b5df87a7f580dfb3991c
yvandy <vandanagarg205@gmail.com>
2021-01-03 16:56:23 +0530

    class using function2

---
+++ Day_12/class_function2.js
@@ -0,0 +1,24 @@
+function Range(from,to){
+    this.from = from;
+    this.to = to;
+}
+
+Range.prototype = {
+
+    includes: function(x){ return this.from <= x && x<= this.to;},
+
+    [Symbol.iterator]: function*(){
+
+        for(let x=Math.ceil(this.from); x<= this.to; x++)
+            yield x;
+    }
+    toString: function(){
+        return"(" + this.from + "..." + this.to +")" ;
+    }
+};
+
+let r = new Range(1,4);
+r.includes(3);
+r.toString();
+[...r]
+
