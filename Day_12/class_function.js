4bfc86efdbfe23499246d3a5a556678865efd004
yvandy <vandanagarg205@gmail.com>
2021-01-03 16:46:02 +0530

    CLASSES using function

---
+++ Day_12/class_function.js
@@ -0,0 +1,21 @@
+function range(from,to){
+    let r = Object.create(range.methods);
+    r.from = from;
+    r.to = to;
+    return r;
+}
+
+range.methods = {
+    includes(x) { return this.from <= x && x<= this.to},
+    *[Symbol.iterator](){
+        for(let x = Math.ceil(this.from); x<=this.to; x++)
+         yield x;         
+    },
+    toString(){
+        return "(" + this.from + "..." + this.to +")";}
+    }
+
+    let r = range(1,4);
+    r.includes(3);
+    r.toString();
+    [...r];
