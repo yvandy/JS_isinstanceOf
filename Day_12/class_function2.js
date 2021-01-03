3250241a508c93cd5f4f853fdb2d5c6b799ad770
yvandy <vandanagarg205@gmail.com>
2021-01-03 17:06:39 +0530

    classes using functions

--- Day_12/class_function2.js
+++ Day_12/class_function2.js
@@ -11,14 +11,14 @@ Range.prototype = {
 
         for(let x=Math.ceil(this.from); x<= this.to; x++)
             yield x;
-    }
+    },
     toString: function(){
         return"(" + this.from + "..." + this.to +")" ;
     }
 };
 
 let r = new Range(1,4);
-r.includes(3);
-r.toString();
-[...r]
+console.log(r.includes(3));
+console.log(r.toString());
+console.log([...r]);
 
