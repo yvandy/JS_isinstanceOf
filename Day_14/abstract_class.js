7876a3f712858533801f4a7aaf62220157219173
yvandy <vandanagarg205@gmail.com>
2021-01-05 23:07:41 +0530

    Abstract Class

---
+++ Day_14/abstract_class.js
@@ -0,0 +1,28 @@
+class AbstractSet{
+
+    has(x) { throw new Error("Abstract method");}
+}
+
+class NotSet extends AbstractSet{
+
+    constructor(set){
+        super();
+        this.set = set;
+    }
+
+    has(x){ return `{x|x not belongs to {this.set.toString()}}`;}
+}
+
+class RangeSet extends AbstractSet{
+    constructor(from,to){
+        super();
+        this.from = from;
+        this.to = to;
+    }
+
+    has(x){ return x>= this.from && x<= this.to;}
+    toString(){ return `{x| ${this.from} <= x <= ${this.to}}`;}
+}
+
+NotSet1 = new NotSet(90);
+console.log(NotSet.has);
