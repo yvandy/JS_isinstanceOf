75d7e420facb2cd3952b91d27bf6b4b7c9abfcab
yvandy <vandanagarg205@gmail.com>
2021-01-06 23:33:34 +0530

    Impor Export

---
+++ Day_15/named_export/mobile.js
@@ -0,0 +1,22 @@
+//named export
+
+export class Nokia{ // it can also be written as export {Nokia}; in different line
+    volumeUp(){
+        console.log("high volume");
+    }
+}
+
+
+
+/* named export for function
+
+fuction show(){
+    console.log("hello module")
+}
+
+*/
+
+/*********************** Export variable****************************
+export const a = 10;
+
+*/
