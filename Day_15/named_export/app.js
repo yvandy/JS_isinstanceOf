790b3128e4276dc36a61ebdf35de457374cbd732
yvandy <vandanagarg205@gmail.com>
2021-01-06 23:35:23 +0530

    Named EXport

---
+++ Day_15/named_export/app.js
@@ -0,0 +1,20 @@
+// Named export class
+
+import {Nokia} from "./mobile.js"
+
+const n = new Nokia();
+n.volumeUp();
+
+/*
+****************************Export function*************
+import {show} from "./mobile.js";
+
+show();
+
+*/
+
+/************************ import variable ****************************
+import {a} from "./mobile.js";
+console.log(a);
+
+*/
