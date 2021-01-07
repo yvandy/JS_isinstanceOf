cd057a8ed5235d1c9359bf3a1c4eb1fbdc3a68b0
yvandy <vandanagarg205@gmail.com>
2021-01-07 15:04:08 +0530

    Default and named export

---
+++ Day_17/mobile.js
@@ -0,0 +1,16 @@
+class Nokia{
+    volumeUp(){
+        console.log("Hi in function");
+
+    }        
+}
+
+function show(){
+    console.log("Hello Module");
+}
+
+const a =10;
+
+export default Nokia;
+export {show, a};
+
