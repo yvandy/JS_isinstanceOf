9a6756b538471fb97aec17c2b16a7a71138481e3
yvandy <vandanagarg205@gmail.com>
2021-01-14 17:27:48 +0530

    async await

--- Day_20/async_await.js
+++ Day_20/async_await.js
@@ -10,11 +10,29 @@ const calculate = (a,b,c) => {
 }
 
 
-calculate(1,2,3).then((addition)=>{
-    console.log(addition);
-    return calculate(addition,4,5)
-}).then((result)=>{
-    console.log(result)
-}).catch((e)=>{
-    console.log(e);
-})
+const add = async () =>{                //expects a promise to be returned within this function
+    const sum1 = await calculate(1,2,3);
+    const sum2 = await calculate(sum1,4,5);
+    const sum3 = await calculate(sum2,6,7);
+    const sum4 = await calculate(sum3,8,9);
+    return sum2;
+}
+
+add().then((value) => console.log(value)).catch((e) => console.log(e));
+//console.log(add())
+
+
+
+
+
+
+//+++++++++++++++++++++ promise channing +++++++++++++++++++++
+
+// calculate(1,2,3).then((addition)=>{
+//     console.log(addition);
+//     return calculate(addition,4,5)
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e);
+// })
